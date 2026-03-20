695cfe16b73d962f9723432f6c0ed83299b24dbd
root <root@vultr.guest>
2026-03-20 12:54:17 +0530

    fix: leg prices × contracts

--- dashboard.py
+++ dashboard.py
@@ -359,20 +359,21 @@ def api_state():
             # add live leg prices if available
             if ps:
                 eth_p = snap.get("eth_price", 0)
+                c = ps.get("contracts", 1)
                 t["legs"] = [
                     {
                         "name": "Short call",
-                        "entry_usd": t.get("call_premium", 0) * eth_p,
-                        "bid_usd": ps.get("live_sc", {}).get("bid", 0) * eth_p,
-                        "ask_usd": ps.get("live_sc", {}).get("ask", 0) * eth_p,
-                        "mid_usd": ps.get("live_sc", {}).get("mid", 0) * eth_p,
+                        "entry_usd": t.get("call_premium", 0) * eth_p * c,
+                        "bid_usd": ps.get("live_sc", {}).get("bid", 0) * eth_p * c,
+                        "ask_usd": ps.get("live_sc", {}).get("ask", 0) * eth_p * c,
+                        "mid_usd": ps.get("live_sc", {}).get("mid", 0) * eth_p * c,
                     },
                     {
                         "name": "Short put",
-                        "entry_usd": t.get("put_premium", 0) * eth_p,
-                        "bid_usd": ps.get("live_sp", {}).get("bid", 0) * eth_p,
-                        "ask_usd": ps.get("live_sp", {}).get("ask", 0) * eth_p,
-                        "mid_usd": ps.get("live_sp", {}).get("mid", 0) * eth_p,
+                        "entry_usd": t.get("put_premium", 0) * eth_p * c,
+                        "bid_usd": ps.get("live_sp", {}).get("bid", 0) * eth_p * c,
+                        "ask_usd": ps.get("live_sp", {}).get("ask", 0) * eth_p * c,
+                        "mid_usd": ps.get("live_sp", {}).get("mid", 0) * eth_p * c,
                     }
                 ]
 
